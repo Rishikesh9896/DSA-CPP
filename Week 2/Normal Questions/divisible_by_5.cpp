2234e58de8f0869a80d3e89e1b0edb351e2ecd59
Rishikesh9896 <rishikeshkapoor1@gmail.com>
2024-09-29 00:26:23 +0530

    Adding new questions

---
+++ Week 2/Normal Questions/divisible_by_5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter the number ";
+    cin>>num;
+    if(num%5==0){
+        cout<<"The number is divisible by 5 ";
+    }
+    else{
+        cout<<"The number is not divisible by 5";
+    }
+}
---
+++ Week 2/Normal Questions/evenOdd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter the number ";
+    cin>>num;
+    if(num%2==0){
+        cout<<"The number entered is even ";
+    }
+    else{
+        cout<<"The number entered is odd ";
+    }
+}
