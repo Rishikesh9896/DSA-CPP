e369defcb14b352aef708a3cca63aa19ff8d9447
Rishikesh9896 <rishikeshkapoor1@gmail.com>
2024-09-29 00:06:17 +0530

    Adding basic questions

---
+++ Week 1/addition.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the valur of a:";
+    cin>>a;
+    cout<<"Enter the value of b:";
+    cin>>b;
+    int addition = a+b;
+    cout<<"The addition of two numbers is "<<addition<<endl;
+}
---
+++ Week 1/swap.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+    cout<<"a:";
+    cin>>a;
+    cout<<"b:";
+    cin>>b;
+
+    int c;
+    c = a;
+    a = b;
+    b = c;
+    
+
+    cout<<"After Swapping a:"<<a<<endl;
+    cout<<"After swapping b:"<<b<<endl;
+
+
+
+}
