fbdc9466a24f0ad5ea30e01a6a0b817626eb4a3b
Rishikesh9896 <rishikeshkapoor1@gmail.com>
2024-09-29 22:10:18 +0530

    New change

--- Week 1/addition.cpp
+++ Week 1/addition.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main(){
     int a,b;
-    cout<<"Enter the valur of a:";
+    cout<<"Enter the valur of a: ";
     cin>>a;
-    cout<<"Enter the value of b:";
+    cout<<"Enter the value of b: ";
     cin>>b;
     int addition = a+b;
     cout<<"The addition of two numbers is "<<addition<<endl;
