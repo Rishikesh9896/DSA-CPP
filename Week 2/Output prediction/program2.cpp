e86bd3d137e5a3c054d59fd64bb03fe3cee3c2dd
Rishikesh9896 <rishikeshkapoor1@gmail.com>
2024-10-02 23:59:50 +0530

    Predict the output

---
+++ Week 2/Output prediction/program2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){ 
+float a = 5, b = 2; 
+int c; 
+c = a % b; 
+cout<<c; 
+return 0; 
+}
+
+// will give an error because we are declaring value of a and b as float and giving the integer which is not true
