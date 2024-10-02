fa045c74ec1fdb51936973580fed98a71c27fd3e
Rishikesh9896 <rishikeshkapoor1@gmail.com>
2024-10-03 00:08:59 +0530

    New change

--- Week 2/Output prediction/program3.cpp
+++ Week 2/Output prediction/program3.cpp
@@ -6,3 +6,8 @@ int b = 5;
 a++, b--;
 cout << ++a << "" "" << b--;
 }
+
+// firstly a++ is incremented which becomes 5 and b-- means decremented which will become 4 
+//when printing 5 value of a is incremented first and then gets printed as 6 and b-- is being printed as 4 
+
+//Therefore the output is 64
