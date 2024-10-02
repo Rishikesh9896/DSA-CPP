f926a249ccb5cf05d547f14a87f643b92feb64a1
Rishikesh9896 <rishikeshkapoor1@gmail.com>
2024-10-02 18:46:28 +0530

    Basic question

--- Week 2/Basic Questions/circumferenceOfCircle.cpp
+++ Week 2/Basic Questions/circumferenceOfCircle.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
  int radius = 10;
- cout<<"Given the radius of circle as 10";
+ cout<<"Given the radius of circle as 10"<<endl;
  float circum;
  circum = 2*3.14*radius;
 cout<<"The circumference is "<<circum<<endl;
